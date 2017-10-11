bf268c5e6ee4bfa55f89a7c610ff1bb1914a9cb4
Bobby <karma0@gmail.com>
2017-10-11 12:05:40 -0600

    Added 'echo' algorithm

--- strategies/mm.py
+++ strategies/mm.py
@@ -3,6 +3,7 @@ Simple Market Maker strategy
 """
 
 from strategies.strategy import IStrategy
+from algorithms.echo import echo
 
 
 class MarketMaker(IStrategy):
@@ -12,7 +13,8 @@ class MarketMaker(IStrategy):
         Bind the strategy to the middleware pipeline,
         returning the context
         """
-        print("ALGO!")
+        echo("ALGO!")
+        # currently, just a pass-through
         return context
 
     def shutdown(self):
