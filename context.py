a75929ae3bacb86ccb4b64e8dd2d8fd8ec376ed1
Bobby <karma0@gmail.com>
2017-10-12 11:22:09 -0600

    Linted

--- context.py
+++ context.py
@@ -2,10 +2,12 @@
 Context for passing through middleware modules
 """
 
-from api.result_types import RESULT_TYPES
-
 
 class Context(dict):
+    """
+    We want our context to act like a dictionary, but references to work with
+    dot-notation.
+    """
     __getattr__ = dict.__getitem__
     __setattr__ = dict.__setitem__
     __delattr__ = dict.__delitem__
@@ -13,14 +15,4 @@ class Context(dict):
 
 def build_context(api_call_results):
     """Assemble and return a context"""
-    #ctx = namedtuple(  # empty class accompishes similar
-
-    # Ingest results, building a context object
-    #ctx.__dict__.update(api_call_results)
-
-    #print(f"RES: {api_call_results}")
-    #for call, result in api_call_results.items():
-    #    setattr(ctx, call, result)
-    #    ctx.__setattr__(call, result)
-
     return Context(api_call_results)
