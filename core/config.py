354a1f7f2743b5c5512632565d24965d2e8eb327
Bobby <karma0@gmail.com>
2017-10-17 12:02:18 -0600

    Dressed up Conf to work with ConfSchema

--- core/config.py
+++ core/config.py
@@ -1,9 +1,9 @@
 """Configuration module"""
 
 from collections import namedtuple
-import json
 
 from common.singleton import Singleton
+from generics.config import ConfSchema
 
 
 Credentials = namedtuple('Credentials', ('api', 'secret'))
@@ -11,51 +11,89 @@ Credentials = namedtuple('Credentials', ('api', 'secret'))
 
 class Conf(metaclass=Singleton):
     """Loads a sane configuration"""
+    sani_data = None
+
     def __init__(self, filename=None):
         if filename is None:
             filename = "config.json"
 
         try:
             with open(filename) as data_file:
-                self.data = json.load(data_file)
+                self.data = data_file.read()
         except:
             raise Exception(f"Could not source config file: {filename}")
+
+        self.conf_schema = ConfSchema()
+        self.sani_data = self.conf_schema.loads(self.data).errors
+        if self.sani_data.errors:
+            raise Exception(f"Could not parse config file \
+                    ({filename}):\n{self.sani_data.errors}")
+
+        self.services_by_name = {}  # type: dict
+
+    def get_api_services_by_name(self):
+        """Return a dict of services by name"""
+        if not self.services_by_name:
+            self.services_by_name = {s.get('name'): s for s in self.sani_data
+                                     .get("api")
+                                     .get("services")}
+        return self.services_by_name
 
     def get_api_credentials(self, apiname):
         """Returns a Credentials object for API access"""
         try:
             return Credentials(
-                api=self.data["api"]["services"][apiname]["credentials"]["apikey"],
-                secret=self.data["api"]["services"][apiname]["credentials"]["secret"],
+                api=self.data
+                .get("api")
+                .get("services")
+                .get(apiname)
+                .get("credentials")
+                .get("apikey")
+                .copy(),
+
+                secret=self.data
+                .get("api")
+                .get("services")
+                .get(apiname)
+                .get("credentials")
+                .get("secret")
+                .copy(),
                 )
-        except:
+        except AttributeError:
             raise Exception(f"Couldn't find credentials for API: {apiname}")
 
     def get_api_endpoints(self, apiname):
         """Returns the API endpoints"""
         try:
-            return self.data["api"]["services"][apiname]["endpoints"].copy()
-        except KeyError:
+            return self.services_by_name\
+                    .get(apiname)\
+                    .get("endpoints")\
+                    .copy()
+        except AttributeError:
             raise Exception(f"Couldn't find the API endpoints")
 
     def get_currencies(self):
         """Returns the currencies that we'll be working with"""
         try:
-            return self.data["currencies"].copy()
-        except KeyError:
-            raise Exception(f"Couldn't find the currencies in the configuration")
+            return self.sani_data.get("currencies").copy()
+        except AttributeError:
+            raise Exception(f"Couldn't find the currencies in the \
+                            configuration")
 
     def get_ws_subscriptions(self, apiname):
         """Returns the websocket subscriptions"""
         try:
-            return self.data["api"]["services"][apiname]["subscriptions"].copy()
-        except KeyError:
+            return self.services_by_name\
+                    .get(apiname)\
+                    .get("subscriptions")\
+                    .copy()
+        except AttributeError:
             raise Exception(f"Couldn't find the websocket subscriptions")
 
     def get_api_calls(self):
         """Returns a list of calls to the api to generate the context object"""
         try:
-            return self.data["api"]["calls"].copy()
+            return self.sani_data.get("api").get("calls").copy()
         except:
             raise Exception(f"Couldn't find call list for APIs")
 
@@ -63,17 +101,17 @@ class Conf(metaclass=Singleton):
         """Returns the API configuration"""
         if name is None:
             try:
-                return self.data["api"].copy()
+                return self.sani_data.get("api").copy()
             except:
                 raise Exception(f"Couldn't find the API configuration")
         try:
-            return self.data["api"]["services"][name].copy()
+            return self.services_by_name.get(name).copy()
         except:
             raise Exception(f"Couldn't find the API configuration")
 
     def get_logger(self, name=None):
         """Return a logger configuration object"""
-        logconf = self.data["logger"].copy()
+        logconf = self.sani_data.get("logger").copy()
         upd = logconf.pop('modules', None)
         try:
             logconf.update(upd[name])
