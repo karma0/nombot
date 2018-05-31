545f04b7d90c34baa5f4c918a414e524a387879b
Bobby <karma0@gmail.com>
2018-05-31 11:20:58 -0600

    Granted currency configuration added flexibility

--- nombot/app/config.py
+++ nombot/app/config.py
@@ -19,10 +19,13 @@ class NomAppConf(AppConf):
         except AttributeError:
             raise Exception(f"Couldn't find credentials for API: {apiname}")
 
-    def get_currencies(self):
+    def get_currencies(self, service=None):
         """Returns the currencies that we'll be working with"""
         try:
+            if service is not None:
+                svc_conf = self.conf.services_by_name[service]
+                return svc_conf.get("currencies", []).copy() \
+                    + self.conf.get("currencies").copy()
             return self.conf.get("currencies").copy()
         except AttributeError:
-            raise Exception(f"Couldn't find the currencies in the \
-                            configuration")
+            return []
