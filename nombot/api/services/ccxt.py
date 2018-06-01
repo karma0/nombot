75fdca65a4e181a26e2d9a0a4a6a194fdcf4af97
Bobby <karma0@gmail.com>
2018-06-01 15:38:06 -0600

    Worked out the major bugs of the asyncio integrations

--- nombot/api/services/ccxt.py
+++ nombot/api/services/ccxt.py
@@ -17,12 +17,20 @@ from nombot.generics.response import ResponseSchema
 class CCXTExchange:
     """Exchange data object"""
     name: str
-    symbols: list
+    currencies: list
     rate_limit: int = None
 
     _ex = None
-    markets = {}  # type: dict
-    queue = asyncio.Queue()
+    loop = asyncio.get_event_loop()
+
+    avail_currencies = None
+    _currencies = None
+
+    avail_markets = None
+    markets = None
+
+    avail_symbols = None
+    symbols = None
 
     def __post_init__(self):
         # instantiate exchange object
@@ -32,30 +40,66 @@ class CCXTExchange:
             self._ex.rate_limit = self.rate_limit
         self._ex.enable_rate_limit = True
 
-        asyncio.get_event_loop().run_until_complete(self.load())
+        self.loop.run_until_complete(self.load())
 
-        if not self.symbols:
-            self.symbols = getattr(self._ex, "symbols", [])
+    async def load(self, reload=False, *args, **kwargs):
+        """Load the markets; populating the exchange object with data"""
+        if self.markets is not None and not reload:
+            return
 
-    async def load(self, *args, **kwargs):
-        """Load the markets; initializing the exchange object with data"""
-        markets = await self._ex.load_markets(*args, **kwargs)
-        self.markets = {  # is this the fastest?
-            "/".join(pair): markets["/".join(pair)]
-            for pair in product(self.symbols, self.symbols)
-            if pair in markets
+        self.avail_markets = await self._ex.load_markets(*args, **kwargs)
+
+        self.avail_currencies = getattr(self._ex, "currencies", {})
+        if not self.currencies:
+            # copy all currencies
+            self._currencies = self.avail_currencies
+        else:
+            # copy relevant currencies
+            self._currencies = {
+                curr: self.avail_currencies[curr]
+                for curr in self.currencies
+                if curr in self.avail_currencies
+            }
+
+        self.avail_symbols = getattr(self._ex, "symbols", [])
+        currencies = self._currencies.keys()
+        self.symbols = [
+            "/".join(pair)
+            for pair in product(currencies, currencies)
+            if "/".join(pair) in self.avail_symbols
+        ]
+
+        self.markets = {
+            sym: self.avail_markets[sym]
+            for sym in self.symbols
         }
+
+    async def call_over_syms(self, callname, *args, **kwargs):
+        """Cycle through configured exchanges and symbols and make a call"""
+        results = {}
+        for sym in self.markets.keys():
+            try:
+                results[sym] = await self.call(callname, sym, *args, **kwargs)
+            except (ExchangeNotAvailable, ExchangeError):
+                pass
+        return results
 
     async def call(self, callname, *args, **kwargs):
         """Generalized async `call` method, pass callname and parameters"""
         try:
-            await getattr(self._ex, callname)(*args, **kwargs)
+            return await getattr(self._ex, callname)(*args, **kwargs)
         except TypeError:
             raise AttributeError(f"Failed to execute call {callname} on "
                                  f"exchange {self._ex.name}")
 
-    def close(self):
-        self._ex.close()
+    async def close(self):
+        """Close all exchange connections"""
+        await self._ex.close()
+
+    def shutdown(self):
+        """Teardown the aio loop"""
+        self.loop.run_until_complete(self.close())
+        self.loop.close()
 
 
 class CCXT:
@@ -69,28 +113,33 @@ class CCXT:
         for exch in exchanges:
             self._ex[exch] = CCXTExchange(exch, symbols, rate_limit)
 
-    async def call_on_exchanges(self, callname, *args, **kwargs):
+    def call_on_exchanges(self, callname, *args, **kwargs):
         """Cycle through all configured exchanges to make a call"""
-        for exchange, ex in self._ex.items():
+        results = {}
+        for ex in self._ex.values():
             try:
-                await ex.call(callname, *args, **kwargs)
+                results[ex.name] = ex.loop.run_until_complete(
+                    ex.call(callname, *args, **kwargs))
             except (ExchangeNotAvailable, ExchangeError):
                 pass
+        return results
 
-    async def call_over_syms(self, callname, *args, **kwargs):
+    def call_over_syms(self, callname, *args, **kwargs):
         """Cycle through configured exchanges and symbols and make a call"""
-        for exchange, ex in self._ex.items():
-            for sym, mkt in ex.markets.items():
-                print(f"""Exchange: {exchange}; Symbol: {sym}""")
-                try:
-                    await ex.call(callname, sym, *args, **kwargs)
-                except (ExchangeNotAvailable, ExchangeError):
-                    pass
+        results = {}
+        for ex in self._ex.values():
+            try:
+                results[ex.name] = ex.loop.run_until_complete(
+                    ex.call_over_syms(callname, *args, **kwargs))
+            except (ExchangeNotAvailable, ExchangeError):
+                pass
+        return results
+
 
     def shutdown(self):
         """Shutdown / cleanup"""
         for exch, ex in self._ex.items():
-            ex.close()
+            ex.shutdown()
 
 
 class CCXTApi(LoggerMixin):  # pylint: disable=R0902
@@ -125,14 +174,15 @@ class CCXTApi(LoggerMixin):  # pylint: disable=R0902
     def call(self, callname, *args, **kwargs):
         """Substitute for REST api as defined in bors.api.requestor.Req"""
         method = self.local_overrides.get(
-                callname, self.local_overrides["default"])
-        loop = asyncio.get_event_loop()
-        results = []
-        loop.run_until_complete(
-                getattr(self.ccxt, method)(callname, *args, **kwargs))
-        loop.close()
+            callname, self.local_overrides["default"])
+        results = getattr(self.ccxt, method)(callname, *args, **kwargs)
+        return results
 
     def shutdown(self):
         """Perform last-minute stuff"""
         self.log.info(f"Shutting down API interface instance for {self.name}")
+
+        # Take care of any currently running tasks in open loops
+        (task.cancel() for task in asyncio.Task.all_tasks())
+
         self.ccxt.shutdown()
