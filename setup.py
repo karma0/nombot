b1444495c73e1f3c7af4885517b0359567efe817
Bobby <karmma0@gmail.com>
2018-06-22 13:13:19 -0600

    Dropped pip.req for parse_requirements

--- setup.py
+++ setup.py
@@ -7,10 +7,11 @@ https://github.com/karma0/nombot.git
 from os import path
 import codecs
 
-try: # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError: # for pip <= 9.0.3
-    from pip.req import parse_requirements
+def parse_requirements(filename):
+    """load requirements from a file"""
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith('#')]
+
 
 # Always prefer setuptools over distutils
 from setuptools import setup, find_packages
@@ -23,11 +24,9 @@ with codecs.open(path.join(HERE, 'README.rst'), encoding='utf-8') as f:
     LONG_DESCRIPTION = f.read()
 
 # parse_requirements() returns generator of pip.req.InstallRequirement objects
-INSTALL_REQS = parse_requirements("./requirements.txt", session='hack')
-
 # REQS is a list of requirement
 # e.g. ['django==1.5.1', 'mezzanine==1.4.6']
-REQS = [str(ir.req) for ir in INSTALL_REQS]
+REQS = parse_requirements("requirements.txt")
 
 # Arguments marked as "Required" below must be included for upload to PyPI.
 # Fields marked as "Optional" may be commented out.
