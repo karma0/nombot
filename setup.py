d6ef8db0a11d854fc52456163e15a72539f8b31e
Bobby <karmma0@gmail.com>
2018-06-24 22:34:50 -0600

    Bump

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/karma0/nombot',
-    version='2.2.1',
+    version='2.2.2',
     zip_safe=False,
 )
